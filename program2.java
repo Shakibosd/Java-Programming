973e892a97d1d171e9a899d37c43f0896971115a
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-08-11 16:55:07 +0600

    first commit

---
+++ program2.java
@@ -0,0 +1,29 @@
+
+// Java Bangla Tutorials 10 : Variable and data type in java (Part-2)
+
+public class program2 {
+ public static void main(String[] args){
+    
+       System.out.println("\n");
+       boolean o = false;
+       byte b = 4;
+       char a = 's';
+       short s = 1234;
+       int i = 10;
+       long l = 6789;
+       float f =  3.764f;
+       double d = 6.967;
+
+       System.out.println("boolean = "+o);
+       System.out.println("byte = "+b);
+       System.out.println("char = "+a);
+       System.out.println("short = "+s);
+       System.out.println("int = "+i);
+       System.out.println("long = "+l);
+       System.out.println("float = "+f);
+       System.out.println("double = "+d);
+
+
+ }   
+}
+
