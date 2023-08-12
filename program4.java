acf0cd12795b83cbc542ec5fa6a1722b2975e197
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-08-12 22:51:12 +0600

    finsh

---
+++ program4.java
@@ -0,0 +1,21 @@
+
+//Java Bangla Tutorials 13 : How to get input from the user
+import java.util.Scanner;
+public class program4 {
+      public static void main(String[] args){
+                 
+            System.out.println("\n");     
+
+
+            Scanner input = new Scanner(System.in);
+            int number;
+
+            System.out.printf("Enter Any Number :  ");
+            number = input.nextInt();
+
+            System.out.println("Number = "+number);
+
+
+      }   
+  }
+
