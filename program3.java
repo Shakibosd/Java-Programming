d66930d66dcfc4789803067fa7084a00e7385942
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-08-12 20:42:00 +0600

    first commit

---
+++ program3.java
@@ -0,0 +1,22 @@
+
+//Java Bangla Tutorials 12 : Format specifier
+
+public class program3 {
+      public static void main(String[] args){
+         
+            System.out.println("\n");
+            boolean b = false;
+            char c = 's';
+            short s = 1234;
+            int i = 10;
+            float f =  3.764f;
+            double d = 6.967;
+     
+            System.out.printf("boolean b = %b\n",b);
+            System.out.printf("char c = %c\n",c);
+            System.out.printf("short s = %d\n",s);
+            System.out.printf("int i = %d\n",i);
+            System.out.printf("float f = %.3f\n",f);
+            System.out.printf("double d = %.3f",d);
+      }   
+}
