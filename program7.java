3e7042b786da018878d5f667ad9be30ebd71df29
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-08-13 20:24:47 +0600

    first commit

---
+++ program7.java
@@ -0,0 +1,31 @@
+// Java Bangla Tutorials 17 : Assignment Operator
+
+public class program7 {
+      public static void main(String[] args){
+         
+            System.out.println("\n");
+
+            int x = 3;
+            int y = 2;
+
+            x+=y;
+            System.out.println("X = "+x);
+
+            x-=y;
+            System.out.println("X = "+x);
+
+            x*=y;
+            System.out.println("X = "+x);
+
+            x/=y;
+            System.out.println("X = "+x);
+
+            x%=y;
+            System.out.println("X = "+x);
+      }   
+
+}
+
+
+
+
