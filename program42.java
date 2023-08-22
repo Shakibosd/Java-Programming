36475de9ed4ef42cc398c96c5373b713a357d8fc
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-08-22 20:05:45 +0600

    finsh'

---
+++ program42.java
@@ -0,0 +1,33 @@
+//Java Bangla Tutorials 64 : How to check an Armstrong Number (practical)
+
+import java.util.Scanner;
+
+public class program42 {
+ public static void main(String[] args) {
+  System.out.println();
+
+  /* int number, sum=0, r, temp;
+  Scanner input = new Scanner(System.in);
+  System.out.printf("Enter Any Number :  ");
+  number = input.nextInt();
+
+  temp = number;
+  
+  while(temp!=0){
+   r = temp % 10;
+   sum = sum + r * r * r;
+   temp = temp / 10;
+  }
+ if(number==sum){
+   System.out.println("Armstrong Number");
+ }
+ else{
+  System.out.println("Not Armstrong");
+ }*/
+
+
+ for(int i=1; i<=1000; i++){
+  System.out.println("Labid");
+ }
+ } 
+}
